50becbe483d814061634eab33ceb5e7fed671901
MiguelA536 <2410307@alumnos.univo.edu.mx>
2025-04-12 12:43:28 -0600

    se agrego el componente cuerpo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChamoyComponent } from "./chamoy/chamoy.component";
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChamoyComponent],
+  imports: [RouterOutlet, ChamoyComponent, CuerpoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
